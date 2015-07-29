faf7f32d29dbb9d11faaad918f8d3241fa8579cf
Francesco Pantisano <pantixkr@gmail.com>
2015-07-29 09:24:01 +0200

    Update jquery.select-multiple.js

--- js/jquery.select-multiple.js
+++ js/jquery.select-multiple.js
@@ -2,11 +2,6 @@
 * SelectMultiple v 0.1
 * Copyright (c) 2015 Francesco Pantisano Original By Louis Cuny v 0.9.11
 *
-* This program is free software. It comes without any warranty, to
-* the extent permitted by applicable law. You can redistribute it
-* and/or modify it under the terms of the Do What The Fuck You Want
-* To Public License, Version 2, as published by Sam Hocevar. See
-* http://sam.zoy.org/wtfpl/COPYING for more details.
 */
 
 !function ($) {
